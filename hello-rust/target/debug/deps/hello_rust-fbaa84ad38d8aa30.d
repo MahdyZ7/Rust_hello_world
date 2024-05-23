18a0769e50be0e410ddd8a4cafdbf5e0c8e59e05
MahdyZ7 <amahdy9@gamil.com>
2024-05-23 19:23:19 +0400

    first rust program

---
+++ hello-rust/target/debug/deps/hello_rust-fbaa84ad38d8aa30.d
@@ -0,0 +1,5 @@
+/home/ayassin/developer/mastery/Rust_hello_world/hello-rust/target/debug/deps/libhello_rust-fbaa84ad38d8aa30.rmeta: src/main.rs
+
+/home/ayassin/developer/mastery/Rust_hello_world/hello-rust/target/debug/deps/hello_rust-fbaa84ad38d8aa30.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/ayassin/developer/mastery/Rust_hello_world/hello-rust/target/debug/hello-rust: /home/ayassin/developer/mastery/Rust_hello_world/hello-rust/src/main.rs
