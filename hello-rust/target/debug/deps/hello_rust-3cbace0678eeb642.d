f92a3bd8f663a4882a7906f48c2b122482173f05
MahdyZ7 <amahdy9@gamil.com>
2024-05-24 23:40:43 +0400

    rmoved target files

--- hello-rust/target/debug/deps/hello_rust-3cbace0678eeb642.d
+++
@@ -1,5 +0,0 @@
-/home/ayassin/developer/mastery/Rust_hello_world/hello-rust/target/debug/deps/hello_rust-3cbace0678eeb642: src/main.rs
-
-/home/ayassin/developer/mastery/Rust_hello_world/hello-rust/target/debug/deps/hello_rust-3cbace0678eeb642.d: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/ayassin/developer/mastery/Rust_hello_world/hello-rust/target/debug/hello-rust: /home/ayassin/developer/mastery/Rust_hello_world/hello-rust/src/main.rs
